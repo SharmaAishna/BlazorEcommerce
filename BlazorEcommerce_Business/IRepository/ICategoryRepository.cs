c40d882ca449fdcb18e78b458745959da07fca27
Aishna Sharma <aishna_sharma@live.com>
2023-10-20 16:41:08 +0200

    Business Project, Common Project, DataAccess Project added Category model and categoryDTO added

---
+++ BlazorEcommerce_Business/IRepository/ICategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using EcommerceModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorEcommerce_Business.IRepository
+{
+    public interface ICategoryRepository
+    {
+        public CategoryDTO Create(CategoryDTO objDTO);
+        public CategoryDTO Update(CategoryDTO objDTO);
+        public CategoryDTO Delete(int id);
+        public List<CategoryDTO> GetAll();
+        public CategoryDTO GetById(int id);
+    }
+}

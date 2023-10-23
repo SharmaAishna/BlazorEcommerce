d6ca022210cf9ea4f563ef07aa822ff374c60ca6
Aishna Sharma <aishna_sharma@live.com>
2023-10-23 10:47:13 +0200

    Categories listed

--- BlazorEcommerce_DataAccessLayer/Category.cs
+++ BlazorEcommerce_DataAccessLayer/Category.cs
@@ -11,7 +11,7 @@ namespace BlazorEcommerce_DataAccessLayer
     {
         [Key]
         public int Id { get; set; }
-        public string? Name { get; set; }
+        public string Name { get; set; }
         public DateTime CreateDate { get; set; }
     }
 }
